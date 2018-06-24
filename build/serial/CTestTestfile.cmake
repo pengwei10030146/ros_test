6a8319a68b20303ffa2f25489b2604995082fcf0
pengwei10030146 <110658513@qq.com>
2018-06-24 19:42:41 +0800

    12345

---
+++ build/serial/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/ros-vm/p14707/serial_ws/src/serial
+# Build directory: /home/ros-vm/p14707/serial_ws/build/serial
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(tests)
